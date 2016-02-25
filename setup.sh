00f36d7aae73f419fdccb5d734f4accea35dc9a4
Ichiro Kimura <kag.web@gmail.com>
2016-02-25 13:04:24 +0900

    Add upgade to pip

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ apt-get install -y python2.7
 apt-get install -y python-pip
 echo "python version $(python --version) running"
 echo "pip version $(pip --version) running"
-pip install pyopenssl
-pip install oauth2client
-pip install google-api-python-client
+pip install --upgrade pyopenssl
+pip install --upgrade oauth2client
+pip install --upgrade google-api-python-client
