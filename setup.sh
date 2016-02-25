efb73cdb356c894a712205f98e490b35eba4e523
Ichiro Kimura <kag.web@gmail.com>
2016-02-25 15:32:46 +0900

    Update libraries

--- setup.sh
+++ setup.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y build-essential
-sudo apt-get install -y libiffi-dev
-sudo apt-get install -y python2.7
-sudo apt-get install -y python-pip
+sudo apt-get install -y build-essential libiffi-dev python-dev python-pip
 echo "python version $(python --version) running"
 echo "pip version $(pip --version) running"
-sudo pip install --upgrade pyopenssl
-sudo pip install --upgrade oauth2client
-sudo pip install --upgrade google-api-python-client
+sudo pip install --upgrade pyopenssl oauth2client google-api-python-client
